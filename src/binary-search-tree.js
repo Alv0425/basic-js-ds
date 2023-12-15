e93a74045803be82627c4d0feea70d8534d768ad
Alv0425 <vilejshikova@gmail.com>
2023-12-15 22:32:14 +0300

    feat: implement bst, quee and remove-from-list tasks

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,39 +8,136 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.rootNode = null;
+  }
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (!this.rootNode) {
+      this.rootNode = new Node(data);
+    } else {
+      let curNode = this.rootNode;
+      let newNode = new Node(data);
+      while (curNode) {
+        if (newNode.data < curNode.data) {
+          if (!curNode.left) {
+            curNode.left = newNode;
+            break;
+          }
+          curNode = curNode.left;
+        } else {
+          if (!curNode.right) {
+            curNode.right = newNode;
+            break;
+          }
+          curNode = curNode.right; 
+        }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let curNode = this.rootNode;
+    while (curNode) {
+      if (data === curNode.data) {
+        return true;
+      }
+      if (data < curNode.data) {
+        if (!curNode.left) {break;}
+        curNode = curNode.left;
+      } else {
+        if (!curNode.right) {break;}
+        curNode = curNode.right;
+      }
+    }
+    return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let curNode = this.rootNode;
+    while (curNode) {
+      if (data === curNode.data) {
+        return curNode;
+      }
+      if (data < curNode.data) {
+        if (!curNode.left) {break;}
+        curNode = curNode.left;
+      } else {
+        if (!curNode.right) {break;}
+        curNode = curNode.right;
+      }
+    }
+    return null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    let curNode = this.rootNode;
+    let parentNode = null;
+    function replace(parent, data, replacer){
+      if (parent) {
+        if(parent.left) {
+          if (parent.left.data === data) {
+            parent.left = replacer;
+          }
+        } 
+        if(parent.right) {
+          if (parent.right.data === data) {
+            parent.right = replacer;
+          }
+        } 
+      }
+    }    
+    while (curNode) {
+      if (data === curNode.data) {
+        if (!curNode.left && !curNode.right) {
+          replace(parentNode, data, null);
+        } else if (curNode.left && !curNode.right) {
+          replace(parentNode, data, curNode.left);
+        } else if (!curNode.left && curNode.right) {
+          replace(parentNode, data, curNode.right);
+        } else {
+          let minInRight = curNode.right;
+          let parentRight = curNode; 
+          while (minInRight.left) {
+            parentRight = minInRight;
+            minInRight = minInRight.left;
+          }
+          replace(parentRight, minInRight.data, minInRight.right);
+          curNode.data = minInRight.data;
+        }
+        break;
+      }
+      if (data < curNode.data) {
+        if (!curNode.left) {break;}
+        parentNode = curNode;
+        curNode = curNode.left;
+      } else {
+        if (!curNode.right) {break;}
+        parentNode = curNode;
+        curNode = curNode.right;
+      }
+    }
+    return null;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let curNode = this.rootNode;
+    while (curNode.left) {
+      curNode = curNode.left;
+    }
+    return curNode.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let curNode = this.rootNode;
+    while (curNode.right) {
+      curNode = curNode.right;
+    }
+    return curNode.data;
   }
 }
 
--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.headElement = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.headElement;
+  }
+
+  enqueue(value) {
+    if (!this.headElement) {
+      this.headElement = new ListNode(value);
+      this.tailElement = new ListNode(value);
+    } else {
+      let curElement  = this.headElement;
+      while (curElement.next) {
+        curElement = curElement.next;
+      }
+      curElement.next = new ListNode(value);
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let removedElement = this.headElement.value;
+    this.headElement = this.headElement.next;
+    return removedElement;
   }
 }
 
